222175b69b391a86c51707155ad87ec8f84f0eed
Romans Gainanovs <gainanov@amazon.com>
2021-05-29 21:23:51 +0100

    lesson2 homework

---
+++ src/main/java/ru/geekbrains/gainanov/homework/lesson2/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.gainanov.homework.lesson2;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException() {
+        super();
+    }
+
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/ru/geekbrains/gainanov/homework/lesson2/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.gainanov.homework.lesson2;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException() {
+        super();
+    }
+
+    public MyArraySizeException(String s) {
+        super(s);
+    }
+}
